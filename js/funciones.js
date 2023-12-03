f7d1a24ce066838de0f2299bc73ea95a3c5aadeb
gonzalocandia92 <gonzaloismael.cg@gmail.com>
2023-12-03 18:47:25 -0300

    Consumo de api en funciones

--- js/funciones.js
+++ js/funciones.js
@@ -1,35 +1,47 @@
-// script.js
-
 const app = Vue.createApp({
-    data() {
-      return {
-        functions: [],
-      };
-    },
-    mounted() {
-      this.fetchFunctions();
-    },
-    methods: {
-      async fetchFunctions() {
-        try {
-          const response = await fetch('https://picsum.photos/v2/list?page=3');
-          const functionsData = await response.json();
-          this.functions = functionsData.map((functionInfo) => ({
-            id: functionInfo.id,
-            author: functionInfo.author,
-            download_url: functionInfo.download_url,
-            description: 'Agregar descripción aquí', // Puedes agregar más detalles según tu necesidad
-          }));
-        } catch (error) {
-          console.error('Error fetching functions:', error);
+  data() {
+    return {
+      funciones: [],
+    };
+  },
+  mounted() {
+    this.fetchFunctions();
+  },
+  methods: {
+    async fetchFunctions() {
+      try {
+        const token = localStorage.getItem("access_token");
+        const headers = new Headers();
+        headers.append("Authorization", `Bearer ${token}`);
+
+        const requestOptions = {
+          method: "GET",
+          headers: headers,
+          redirect: "follow",
+        };
+
+        const response = await fetch(
+          "https://gcandia1992.pythonanywhere.com/funciones",
+          requestOptions
+        );
+
+        if (!response.ok) {
+          throw new Error(
+            `Error al cargar datos de funciones. Código: ${response.status}`
+          );
         }
-      },
-      showFunctionDetails(functionInfo) {
-        // Lógica para mostrar detalles de la función, si es necesario
-        console.log('Detalles de la función:', functionInfo);
-      },
+
+        const data = await response.json();
+        this.funciones = data.funciones;
+      } catch (error) {
+        console.error("Error al cargar datos de funciones:", error);
+      }
     },
-  });
-  
-  app.mount('#app');
-  
+    showFunctionDetails(funcion) {
+      // Lógica para mostrar detalles de la función, si es necesario
+      console.log("Detalles de la función:", funcion);
+    },
+  },
+});
+
+app.mount("#app");
